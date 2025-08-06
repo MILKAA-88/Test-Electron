9f72ffbf2799eda5a7969a69dbb8ab824224c42b
@MILKAA-88 <kameel.2011@icloud.com>
2025-08-06 21:40:06 +0200

    update

--- main.js
+++ main.js
@@ -1,12 +1,27 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, Menu, ipcMain } = require('electron');
+const path = require('path');
 
 function createWindow() {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      contextIsolation: true,
+      nodeIntegration: false,
+    }
   });
   
-  win.loadFile('index.html');
+  Menu.setApplicationMenu(null);
+  win.loadFile('start.html');
+  
+  ipcMain.on('load-page', (event, page) => {
+    win.loadFile(page);
+  });
 }
 
 app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
